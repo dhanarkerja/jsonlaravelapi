b57865971ad8e1130e9423650fdba21e2d875dff
Dhanar Kerja <dhanarkerja@gmail.com>
2022-08-30 16:57:44 +0700

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\MahasiswaController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::get('mahasiswa',[MahasiswaController::class,'index']);
+Route::post('mahasiswa/store',[MahasiswaController::class,'store']);
+Route::get('mahasiswa/show/{id}',[MahasiswaController::class,'show']);
+Route::post('mahasiswa/update/{id}',[MahasiswaController::class,'update']);
+Route::delete('mahasiswa/destroy/{id}',[MahasiswaController::class,'destroy']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
